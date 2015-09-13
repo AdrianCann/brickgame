19ec209c7de1952b3ee0941350a8a0f6f48a6101
kcart <llid09@gmail.com>
2015-09-13 16:00:44 -0400

    troublshooting

--- game1/brickgame.py
+++ game1/brickgame.py
@@ -9,4 +9,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=False, port=33507)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
